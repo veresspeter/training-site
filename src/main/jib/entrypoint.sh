31e9fcc88ff159520207dcf65e78b0f82f9c02a9
Peter Veress <peter.veress@nitrowise.com>
2021-02-08 18:00:35 +0100

    jenkins update

--- src/main/jib/entrypoint.sh
+++ src/main/jib/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "The application will start in ${JHIPSTER_SLEEP}s..." && sleep ${JHIPSTER_SLEEP}
-exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -datasource.username=${DB_USR} -datasource.password=${DB_PSW} -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "hu.redriver.MaxmoveApp"  "$@"
+exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Ddatasource.username=${DB_USR} -Ddatasource.password=${DB_PSW} -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "hu.redriver.MaxmoveApp"  "$@"
