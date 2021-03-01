6061290a82a6f82310e3698ab48f6fba253ff128
Peter Veress <peter.veress@nitrowise.com>
2021-03-01 13:11:38 +0100

    fix
    (cherry picked from commit 7012cc7d862519c8d783cf525cbb397f7983eeed)

--- src/main/java/hu/redriver/service/ZoomAPIClientService.java
+++ src/main/java/hu/redriver/service/ZoomAPIClientService.java
@@ -11,7 +11,6 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.time.Duration;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
