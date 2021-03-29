eb152de3864e23bfbbad9025909f2fa582e93771
Peter Veress <peter.veress@nitrowise.com>
2021-03-29 02:49:19 +0200

    calendar

--- src/main/webapp/app/calendar/event.component.ts
+++ src/main/webapp/app/calendar/event.component.ts
@@ -2,16 +2,12 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpResponse } from '@angular/common/http';
 import { Subscription } from 'rxjs';
 import { JhiEventManager } from 'ng-jhipster';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 import { IEvent } from 'app/shared/model/event.model';
 import { EventService } from '../shared/services/event.service';
-import { EventDeleteDialogComponent } from './event-delete/event-delete-dialog.component';
-import { Moment } from 'moment';
-import * as moment from 'moment';
 import { AccountService } from 'app/core/auth/account.service';
-import { IAppUser } from 'app/shared/model/application-user.model';
 import { ApplicationUserService } from 'app/shared/services/application-user.service';
+import { UserService } from 'app/core/user/user.service';
 
 @Component({
   selector: 'jhi-event',
@@ -19,20 +15,33 @@ import { ApplicationUserService } from 'app/shared/services/application-user.ser
   styleUrls: ['event.component.scss'],
 })
 export class EventComponent implements OnInit, OnDestroy {
+  readonly DAY_TO_MS = 24 * 60 * 60 * 1000;
+  readonly SINGLE_DAY_CALENDAR_MAX_WIDTH = 425;
+
+  authorities: string[] | undefined = [];
   events?: IEvent[];
   eventSubscriber?: Subscription;
   activeUserId: number | undefined;
   isUserRequestActive = false;
   loading = true;
-  saving = false;
+  datePickerFirstDate: Date;
+  datePickerLastDate: Date;
 
   constructor(
     protected eventService: EventService,
     protected eventManager: JhiEventManager,
-    protected modalService: NgbModal,
     protected accountService: AccountService,
-    protected applicationUserService: ApplicationUserService
-  ) {}
+    protected applicationUserService: ApplicationUserService,
+    protected userService: UserService
+  ) {
+    const date = new Date();
+    if (date.getDay() !== 1) {
+      this.datePickerFirstDate = new Date((this.getDayByDate(date) - ((date.getDay() + 6) % 7)) * this.DAY_TO_MS);
+    } else {
+      this.datePickerFirstDate = new Date(this.getDayByDate(date) * this.DAY_TO_MS);
+    }
+    this.datePickerLastDate = new Date(this.datePickerFirstDate.getTime() + 6 * this.DAY_TO_MS);
+  }
 
   loadAll(): void {
     this.eventService.query().subscribe((res: HttpResponse<IEvent[]>) => {
@@ -62,86 +71,6 @@ export class EventComponent implements OnInit, OnDestroy {
     this.eventSubscriber = this.eventManager.subscribe('eventListModification', () => this.loadAll());
   }
 
-  delete(event: IEvent): void {
-    const modalRef = this.modalService.open(EventDeleteDialogComponent, { size: 'lg', backdrop: 'static' });
-    modalRef.componentInstance.event = event;
-  }
-
-  isJoinShown(event: IEvent): boolean {
-    return this.isUserAuthenticated() && !this.isUserRegistered(event.participants);
-  }
-
-  isRegisteredShown(event: IEvent): boolean {
-    return this.isUserAuthenticated() && this.isUserRegistered(event.participants);
-  }
-
-  isUserRegistered(participants: IAppUser[] | undefined): boolean {
-    return participants?.find(participant => participant.id === this.activeUserId) !== undefined;
-  }
-
-  isUserAuthenticated(): boolean {
-    this.checkUserAuthentication();
-    return this.activeUserId !== undefined;
-  }
-
-  isEventInProgress(event: IEvent): boolean {
-    if (!event.start || !event.end) {
-      return false;
-    }
-
-    const toStart = this.getMomentAndNowDiff(event.start);
-    const toEnd = this.getMomentAndNowDiff(event.end);
-
-    return toStart < 15 && toEnd > -15;
-  }
-
-  isEventEditable(event: IEvent): boolean {
-    if (!event.start || !event.end) {
-      return false;
-    }
-
-    return this.getMomentAndNowDiff(event.start) < 180;
-  }
-
-  isEventClosed(event: IEvent): boolean {
-    if (!event.start || !event.end) {
-      return false;
-    }
-
-    return this.getMomentAndNowDiff(event.start) < 180 && this.getMomentAndNowDiff(event.end) > -15;
-  }
-
-  isStartEnabled(event: IEvent): boolean {
-    this.checkUserAuthentication();
-    return this.isEventInProgress(event) && this.isUserRegistered(event.participants) && this.activeUserId !== undefined;
-  }
-
-  joinEvent(eventId: number | undefined): void {
-    this.saving = true;
-    if (eventId !== undefined) {
-      this.eventService.join(eventId).subscribe(
-        () => {
-          this.saving = false;
-          this.eventManager.broadcast('eventListModification');
-        },
-        () => (this.saving = false)
-      );
-    }
-  }
-
-  quitEvent(eventId: number | undefined): void {
-    this.saving = true;
-    if (eventId !== undefined) {
-      this.eventService.quit(eventId).subscribe(
-        () => {
-          this.saving = false;
-          this.eventManager.broadcast('eventListModification');
-        },
-        () => (this.saving = false)
-      );
-    }
-  }
-
   checkUserAuthentication(): void {
     if (this.activeUserId === undefined && this.accountService.isAuthenticated() && !this.isUserRequestActive) {
       this.isUserRequestActive = true;
@@ -150,6 +79,7 @@ export class EventComponent implements OnInit, OnDestroy {
           if (account !== null && account.internalUser!.id !== null) {
             this.applicationUserService.findByInternalId(account.internalUser!.id).subscribe(
               res => {
+                this.authorities = res.body?.internalUser?.authorities;
                 this.activeUserId = res.body?.id;
                 this.isUserRequestActive = false;
               },
@@ -162,7 +92,49 @@ export class EventComponent implements OnInit, OnDestroy {
     }
   }
 
-  getMomentAndNowDiff(time: Moment): number {
-    return moment.duration(time.diff(moment())).asMinutes();
+  getCalendarDates(): Date[] {
+    if (!this.isMultiDate()) {
+      return [new Date()];
+    } else {
+      const dates: Date[] = [];
+      for (let i = 0; i < 7; i++) {
+        dates.push(new Date((this.getDayByDate(this.datePickerFirstDate) + i) * this.DAY_TO_MS));
+      }
+      return dates;
+    }
+  }
+
+  isMultiDate(): boolean {
+    return window.innerWidth > this.SINGLE_DAY_CALENDAR_MAX_WIDTH;
+  }
+
+  decreaseDatePicker(): void {
+    this.datePickerLastDate = new Date((this.getDayByDate(this.datePickerLastDate) - 7) * this.DAY_TO_MS);
+    this.datePickerFirstDate = new Date((this.getDayByDate(this.datePickerFirstDate) - 7) * this.DAY_TO_MS);
+  }
+
+  increaseDatePicker(): void {
+    this.datePickerLastDate = new Date((this.getDayByDate(this.datePickerLastDate) + 7) * this.DAY_TO_MS);
+    this.datePickerFirstDate = new Date((this.getDayByDate(this.datePickerFirstDate) + 7) * this.DAY_TO_MS);
+  }
+
+  getEventsByDay(date: Date): IEvent[] | undefined {
+    return this.events?.filter((event: IEvent) => this.getDayByDate(event?.start?.toDate()) === this.getDayByDate(date));
+  }
+
+  getDayByDate(date: Date | undefined): number {
+    if (date === undefined) {
+      return 0;
+    }
+
+    return Math.floor(date.getTime() / this.DAY_TO_MS + 0.1);
+  }
+
+  isLeftDatePickerDisabled(): boolean {
+    return this.getDayByDate(this.datePickerLastDate) < this.getDayByDate(new Date());
+  }
+
+  emitAuthEvent(): void {
+    this.checkUserAuthentication();
   }
 }
