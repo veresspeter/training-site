475dfe47cfe7c5396861fd8b99f0a65a72b55c44
Peter Veress <peter.veress@nitrowise.com>
2021-04-01 11:10:43 +0200

    refakt and webpack update

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -13,92 +13,97 @@ const commonConfig = require('./webpack.common.js');
 const ENV = 'production';
 const sass = require('sass');
 
-module.exports = webpackMerge(commonConfig({ env: ENV }), {
-    // Enable source maps. Please note that this will slow down the build.
-    // You have to enable it in Terser config below and in tsconfig.json as well
-    // devtool: 'source-map',
-    entry: {
-        global: './src/main/webapp/content/scss/global.scss',
-        main: './src/main/webapp/app/app.main'
-    },
-    output: {
-        path: utils.root('target/classes/static/'),
-        filename: 'app/[name].[hash].bundle.js',
-        chunkFilename: 'app/[id].[hash].chunk.js'
-    },
-    module: {
-        rules: [
-        {
-            test: /\.scss$/,
-            use: ['to-string-loader', 'css-loader', 'postcss-loader', {
-                loader: 'sass-loader',
-                options: { implementation: sass }
-            }],
-            exclude: /(vendor\.scss|global\.scss)/
-        },
-        {
-            test: /(vendor\.scss|global\.scss)/,
-            use: [
-                {
-                    loader: MiniCssExtractPlugin.loader,
-                    options: {
-                        publicPath: '../'
-                    }
-                },
-                'css-loader',
-                'postcss-loader',
-                {
-                    loader: 'sass-loader',
-                    options: { implementation: sass }
-                }
-            ]
-        },
-        {
-            test: /\.css$/,
-            use: ['to-string-loader', 'css-loader'],
-            exclude: /(vendor\.css|global\.css)/
-        },
-        {
-            test: /(vendor\.css|global\.css)/,
-            use: [
-                {
-                    loader: MiniCssExtractPlugin.loader,
-                    options: {
-                        publicPath: '../'
-                    }
-                },
-                'css-loader',
-                'postcss-loader'
-            ]
-        }]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            // Options similar to the same options in webpackOptions.output
-            // both options are optional
-            filename: 'content/[name].[contenthash].css',
-            chunkFilename: 'content/[id].css'
-        }),
-        new MomentLocalesPlugin({
-            localesToKeep: [
-                // jhipster-needle-i18n-language-moment-webpack - JHipster will add/remove languages in this array
-            ]
-        }),
-        new BundleAnalyzerPlugin({
-            analyzerMode: 'static',
-            openAnalyzer: false,
-            // Webpack statistics in target folder
-            reportFilename: '../stats.html'
-        }),
-        new webpack.LoaderOptionsPlugin({
-            minimize: true,
-            debug: false
-        }),
-        new WorkboxPlugin.GenerateSW({
-            clientsClaim: true,
-            skipWaiting: true,
-            exclude: [/swagger-ui/]
-        })
-    ],
-    mode: 'production'
+module.exports = webpackMerge(commonConfig({env: ENV}), {
+  // Enable source maps. Please note that this will slow down the build.
+  // You have to enable it in Terser config below and in tsconfig.json as well
+  // devtool: 'source-map',
+  entry: {
+    global: './src/main/webapp/content/scss/global.scss',
+    main: './src/main/webapp/app/app.main'
+  },
+  output: {
+    path: utils.root('target/classes/static/'),
+    filename: 'app/[name].[hash].bundle.js',
+    chunkFilename: 'app/[id].[hash].chunk.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['to-string-loader', 'css-loader', 'postcss-loader', {
+          loader: 'sass-loader',
+          options: {implementation: sass}
+        }],
+        exclude: /(vendor\.scss|global\.scss)/
+      },
+      {
+        test: /(vendor\.scss|global\.scss)/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../'
+            }
+          },
+          'css-loader',
+          'postcss-loader',
+          {
+            loader: 'sass-loader',
+            options: {implementation: sass}
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ['to-string-loader', 'css-loader'],
+        exclude: /(vendor\.css|global\.css)/
+      },
+      {
+        test: /(vendor\.css|global\.css)/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../'
+            }
+          },
+          'css-loader',
+          'postcss-loader'
+        ]
+      }]
+  },
+
+  optimization: {
+    runtimeChunk: false,
+    minimizer: false
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: 'content/[name].[contenthash].css',
+      chunkFilename: 'content/[id].css'
+    }),
+    new MomentLocalesPlugin({
+      localesToKeep: [
+        // jhipster-needle-i18n-language-moment-webpack - JHipster will add/remove languages in this array
+      ]
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+      // Webpack statistics in target folder
+      reportFilename: '../stats.html'
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: false,
+      debug: false
+    }),
+    new WorkboxPlugin.GenerateSW({
+      clientsClaim: true,
+      skipWaiting: true,
+      exclude: [/swagger-ui/]
+    })
+  ],
+  mode: 'production'
 });
